8a0b16a3e39654fcf270f26e9facc2b4da82ce69
Frieder Erdmann <frieder.erdmann@massive.se>
2023-12-05 09:48:47 +0100

    part 2 brute forced

--- 2023/day_05.py
+++ 2023/day_05.py
@@ -44,23 +44,33 @@ for seed in seeds:
 print(min(destinations))
 
 
+# for sample data
 # destinations = []
 # for i in range(0, len(seeds), 2):
 #     for j in range(seeds[i], seeds[i] + seeds[i+1]):
 #         destinations.append(run_through_mappings(j))
 # print(min(destinations))
 
-for i in range(100): # locations
+part_2 = {seeds[x]: seeds[x+1] for x in range(0, len(seeds), 2)}
+
+# reverse brute force approach, ran with multiple numbers.
+stop = False
+i = 72246198 # guessing reasonable starting number
+while True: # locations
     a = i
     for name in reversed(mappings):
         mapping = mappings[name]
         for (imin, imax), value in mapping.items():
-            if not value <= a <= value + imax - imin:
-                continue
-            a += a-value
+            if value <= a <= value + imax - imin:
+                a = imin + a-value
+                break
+    for start, irange in part_2.items():
+        if start <= a <= start+irange:
+            stop = True
             break
-    if a in seeds:
-        break
+    if stop: break
+    print(i)
+    i += 1
 
 print(i)
 
