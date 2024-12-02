b37879c23fa5be73a41adb73b99e492b9ecd069b
Frieder Erdmann <55715326+friedererdmann@users.noreply.github.com>
2024-12-02 14:52:02 +0100

    Update day_01.py

--- 2024/day_01/day_01.py
+++ 2024/day_01/day_01.py
@@ -1,33 +1,10 @@
 with open("2024/day_01/day_01.txt", "r") as fp:
     lines = fp.readlines()
 
-a = []
-b = []
-
-
-for line in lines:
-    x, y = line.split(" ", 1)
-    a.append(int(x))
-    b.append(int(y))
-
+a, b = list(zip(*[x for x in [map(int, line.split()) for line in lines]]))
 
 # part 1
-a.sort()
-b.sort()
-print(sum([abs(m - n) for m,n in zip(a, b)]))
-
+print(sum([abs(m - n) for m,n in zip(sorted(a), sorted(b))]))
 
 # part 2
-count_a = {}
-for z in list(set(a)):
-    count_a[z] = a.count(z)
-
-count_b = {}
-for z in list(set(b)):
-    count_b[z] = b.count(z)
-
-part_2 = 0
-for key, value in count_a.items():
-    part_2 += key * count_b.get(key, 0) * value
-
-print(part_2)
+print(sum([d * a.count(d) * b.count(d) for d in set(a)]))
