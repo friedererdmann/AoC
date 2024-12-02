c036a4f61841ca2c116a25f85f4788a410dabf7a
Frieder Erdmann <55715326+friedererdmann@users.noreply.github.com>
2024-12-02 13:25:10 +0100

    Update day_02.py

--- 2024/day_02/day_02.py
+++ 2024/day_02/day_02.py
@@ -1,45 +1,22 @@
-with open("2024/day_02/day_02.txt", "r") as fp:
-    lines = fp.readlines()
-
-
 def is_it_good(series):
     good = False
-    dir = 1
-    for i, v in enumerate(series):
-        if i == 0:
-            distance = series[i + 1] - v
-            if distance == 0:
-                break
-            dir = abs(distance)/distance
-            continue
-        distance = v - series[i - 1]
-        if distance == 0:
-            break
-        if dir != abs(distance)/distance:
-            break
-        if 3 < abs(distance):
-            break
-        if i == len(series) - 1:
-            good = True
-    return i, good
+    diff = [series[i + 1] - series[i] for i in range(len(series) - 1)]
+    zeroes = any([x == 0 for x in diff])
+    larger_three = any([abs(x) > 3 for x in diff])
+    direction = [((abs(x)/x) + 1) / 2 for x in diff if x != 0]
+    good = not zeroes and not larger_three and (all(direction) or not any(direction))
+    return good
 
 
-safe = 0
-part_2 = False
+def run_part(part=0):
+    with open("2024/day_02/day_02.txt", "r") as fp:
+        lines = fp.readlines()
+    count = 0
+    for line in lines:
+        series = [int(x) for x in line.split()]
+        count += int(any([is_it_good(series[0:i] + series[i+part:len(series)]) for i in range(len(series))]))
+    return count
 
-for line in lines:
-    series = [int(x) for x in line.split(" ")]
-    idx, success = is_it_good(series)
-    if not success and part_2:
-        for i in range(len(series)):
-            xseries = series.copy()
-            xseries.pop(i)
-            xidx, xsuccess = is_it_good(xseries)
-            if xsuccess:
-                series = xseries
-                success = True
-                break
-    if success:
-        safe += 1
 
-print(safe)
+print(run_part(0))
+print(run_part(1))
