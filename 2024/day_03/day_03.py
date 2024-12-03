1c9385dadcd1537c12cf38f9f354a090b52b8704
Frieder Erdmann <55715326+friedererdmann@users.noreply.github.com>
2024-12-03 06:47:05 +0100

    initial version of day 3

---
+++ 2024/day_03/day_03.py
@@ -0,0 +1,52 @@
+with open("2024/day_03/day_03.txt", "r") as fp:
+    lines = fp.read()
+
+
+def mul(a, b):
+    return a * b
+
+import re
+
+mul_pattern = "mul\\(\\d{1,3},\\d{1,3}\\)"
+statements = re.findall(mul_pattern, lines)
+i = sum([eval(s) for s in statements])
+print(i)
+
+
+
+do_pattern = "do\\(\\)"
+dont_pattern = "don't\\(\\)"
+
+do = True
+nr = 0
+
+search = lines
+while True:
+    index = len(search)
+    c_end = 0
+    instruction = ""
+    breaking = True
+    for pattern in [mul_pattern, do_pattern, dont_pattern]:
+        net = re.search(pattern, search)
+        if not net:
+            continue
+        text = net.group()
+        start, end = net.span()
+        if start < index:
+            index = start
+            c_end = end
+            instruction = text
+            breaking = False
+    if breaking:
+        break
+    if instruction == "don't()":
+        do = False
+    if instruction == "do()":
+        do = True
+    if do and instruction.startswith("mul("):
+        nr += eval(instruction)
+    search = search[c_end:]
+
+print(nr)
+        
+
